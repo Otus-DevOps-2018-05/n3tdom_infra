8d1158a575975aaea50f8d4b22a0edf16dd97fc6
Mikhail Laptev <n3tdom@outlook.com>
2018-07-31 14:32:15 +0300

    base hw done

---
+++ terraform/modules/db/outputs.tf
@@ -0,0 +1,5 @@
+output "db_external_ip" {
+  #google_compute_instance.app = тип ресурса
+  #все что после первой точки - имя ресурса (.network_interface.0.access_config.0.assigned_nat_ip)
+  value = "${google_compute_instance.db.*.network_interface.0.access_config.0.assigned_nat_ip}"
+}
---
+++ terraform/prod/variables.tf
@@ -0,0 +1,44 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-north1"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable zone {
+  description = "Zone"
+}
+
+variable private_key {
+  description = "Path to the private key used for ssh provisioning"
+}
+
+variable instance_count {
+  description = "How many instances to keep"
+  default     = "1"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
+
+variable source_ranges {
+  description = "Allow IP"
+  type        = "list"
+}
